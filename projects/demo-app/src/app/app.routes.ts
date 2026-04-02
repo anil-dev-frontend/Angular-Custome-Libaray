954b4a9da1c2cef76eb5fef0c8d3c8aa03722730
Anil Yadav <133839287+AnilKYad@users.noreply.github.com>
2026-04-02 12:59:59 +0530

    Header and Sidebar

--- projects/demo-app/src/app/app.routes.ts
+++ projects/demo-app/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { InputDemo } from './components/input-demo/input-demo';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'input', pathMatch: 'full' },
+  { path: 'input', component: InputDemo }
+];
--- projects/demo-app/src/app/app.ts
+++ projects/demo-app/src/app/app.ts
@@ -1,13 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { InputDemo } from '../app/components/input-demo/input-demo';
+import { Sidebar } from './components/layout/sidebar/sidebar';
+import { Header } from './components/layout/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, InputDemo],
+  imports: [RouterOutlet, InputDemo,Sidebar,Header],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('demo-app');
+  protected readonly title = signal('Angular-Libarary');
 }
